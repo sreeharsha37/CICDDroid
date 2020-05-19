207f901a48cbb66ef34a25a11896b53f732c93d4
sreeharsha37 <sreeharsha37@gmail.com>
2020-05-19 13:46:02 +0530

    change in mainactivity.kt

--- app/src/main/java/com/sreeharsha37/retirementcalc/MainActivity.kt
+++ app/src/main/java/com/sreeharsha37/retirementcalc/MainActivity.kt
@@ -7,12 +7,13 @@ import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+  class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        AppCenter.start(application, appSecret:"0eac16ae-a9df-43ee-85cf-2e6c11f5a1e9", Analytics::class.java, Crashes::class.java);
+        AppCenter.start(application, "0eac16ae-a9df-43ee-85cf-2e6c11f5a1e9", Analytics::class.java, Crashes::class.java);
+
     }
 }
