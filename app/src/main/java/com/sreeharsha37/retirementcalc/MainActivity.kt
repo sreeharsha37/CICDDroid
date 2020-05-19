4047af1c3a7e54eb82ff6a13b6d0356781de94b1
sreeharsha37 <sreeharsha37@gmail.com>
2020-05-19 10:50:39 +0530

    initial commit

---
+++ app/src/main/java/com/sreeharsha37/retirementcalc/MainActivity.kt
@@ -0,0 +1,17 @@
+  package com.sreeharsha37.retirementcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, appSecret:"0eac16ae-a9df-43ee-85cf-2e6c11f5a1e9", Analytics::class.java, Crashes::class.java);
+    }
+}
