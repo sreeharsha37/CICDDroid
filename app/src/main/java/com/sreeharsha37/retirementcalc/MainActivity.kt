3aef695f413307b64a7aed8153e5edd0aab67087
sreeharsha37 <sreeharsha37@gmail.com>
2020-05-26 11:50:40 +0530

    change in mainactivity.kt

--- app/src/main/java/com/sreeharsha37/retirementcalc/MainActivity.kt
+++ app/src/main/java/com/sreeharsha37/retirementcalc/MainActivity.kt
@@ -2,10 +2,12 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
   class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,33 @@ import kotlinx.android.synthetic.main.activity_main.*
         setContentView(R.layout.activity_main)
 
         AppCenter.start(application, "0eac16ae-a9df-43ee-85cf-2e6c11f5a1e9", Analytics::class.java, Crashes::class.java);
+
+        calculateButton.setOnClickListener{
+            // Crashes.generateTestCrash();
+            try {
+            val interestRate = interestEditText.text.toString().toFloat()
+            val currentAge = ageEditText.text.toString().toInt()
+            val retirementAge = retirementEditText.text.toString().toInt()
+            val monthly = monthlySavingsEditText.text.toString().toFloat()
+            val current = currentEditText.text.toString().toFloat()
+
+            val properties:HashMap<String, String> = HashMap<String, String>()
+            properties.put("interest_rate", interestRate.toString())
+            properties.put("current_age", currentAge.toString())
+            properties.put("retirement_age", retirementAge.toString())
+            properties.put("monthly_savings", monthly.toString())
+            properties.put("current_savings", current.toString())
+
+            if(interestRate <=0){
+                Analytics.trackEvent("wrong_interest_rate", properties);
+            }
+            if(retirementAge <= currentAge){
+                Analytics.trackEvent("wrong_age", properties);
+            }
+        } catch (ex: Exception){
+                Analytics.trackEvent(ex.message);
+            }
+        }
 
     }
 }
